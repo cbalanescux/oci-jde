a6cbf8ce8223ee8f0c01d9add501b083b89dd9c7
cbalanescuy <cezar.balanescu@oracle.com>
2023-04-20 13:03:27 +0300

    Remove config_file_profile
    Remove config_file_profile

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 # Copyright (c) 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-variable "config_file_profile" {
-  default = "DEFAULT"
-}
+#variable "config_file_profile" {
+#  default = "DEFAULT"
+#}
 
 variable "auth_type" {
     default = "instance"
